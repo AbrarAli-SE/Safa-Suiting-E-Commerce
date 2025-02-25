f7d43be9342e00967da22f003e048bd2c01a60a9
Saad123ali <itmesaad@gmail.com>
2025-02-25 23:47:32 +0500

    Update the css

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        red: {
+          500: '#ff0000',
+          700: '#cc0000',
+        },
+      },
+    },
+  },
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
